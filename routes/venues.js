1566fa4e524d97c5150332af21e695977925d17c
kelvinma <me@kelvinma.com>
2015-11-24 13:37:00 -0500

    Gitignore + hiding credentials

--- routes/venues.js
+++ routes/venues.js
@@ -1,35 +1,35 @@
+require('dotenv').load();
+
 var express = require('express');
 var router = express.Router();
 var https = require('https');
 var request = require('request');
+
 
 /* GET venues listing. */
 router.get('/', function(req, res, next) {
   res.send('venues page');
 });
 
-var clientId = 'D3XCQHVESV4KMC2KTYCEKXRYE11U4FZOKPKDLYPZBAZPQE0D';
-var clientSecret = 'TJ1HYBNMZ0MVMA21Z4YVIOT4G3DOALEGFNHLPN4AOY5NPWG5';
-
 router.get('/:near', function(req, res, next){
 
-  var search = 'https://api.foursquare.com/v2/venues/search?near=' + req.params.near +  '&client_id=' + clientId + '&client_secret=' + clientSecret + '&v=20151121';
+  var search = 'https://api.foursquare.com/v2/venues/search?near=' + req.params.near +  '&client_id=' + process.env. + '&client_secret=' + clientSecret + '&v=20151121';
+
+    // var options = req.query
+    // var search = 'https://api.foursquare.com/v2/venues' + options +  '&client_id=' + clientId + '&client_secret=' + clientSecret + '&v=20151121';
 
   var venues = function(){
     request(search, function(err, res, body){
-    if (err){
-      console.error(err);
-    }
-      console.log(JSON.stringify(body));
-      return JSON.stringify(body);
+      if (err){
+        console.error(err);
+      }
     })
     .pipe(res)
     .on('error', function(err){
       console.error(err);
     });
   };
-  res.end(venues());
-
+    venues();
 });
 
 
